32f1eb06beda2abd5ac77ab5fa98a34e3769215a
Soran Raof <soranraof@hotmail.com>
2023-07-26 22:59:15 +0100

    Progress on Navbar

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Soran Raof | Full Stack Developer</title>
+      </Head>
+      <Navbar />
+    </div>
+  );
+}
