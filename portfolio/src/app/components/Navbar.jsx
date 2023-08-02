a82bb4fd7bd54a2c84971f8f389cf0ce931f16f7
Soran Raof <soranraof@hotmail.com>
2023-08-02 17:03:08 +0100

    fixed issue of navbar image not rendering on hosted website

--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Link href={"/"}>
           <Image
-            src="/../public/assets/SR-logo-purple.png"
+            src="/assets/SR-logo-purple.png"
             alt="srlogo"
             width="125"
             height="50"
@@ -70,7 +70,7 @@ const Navbar = () => {
             <div className="flex w-full items-center justify-between">
               <Link href={"/"}>
                 <Image
-                  src="/../public/assets/SR-logo-purple.png"
+                  src="/assets/SR-logo-purple.png"
                   width="87"
                   height="35"
                   alt="srlogo"
