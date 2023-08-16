10b685290a1e5f89b34ca45f859cc291f416e4f1
Soran Raof <soranraof@hotmail.com>
2023-08-16 14:40:38 +0100

    implemented some data and prompts for chatbot

--- portfolio/src/app/helpers/constants/chatbot-prompts.ts
+++ portfolio/src/app/helpers/constants/chatbot-prompts.ts
@@ -1,16 +1,30 @@
-import { bookData } from "./book-data";
+import { soranData } from "./soran-data";
 
 export const chatbotPrompt = `
-You are a helpful customer support chatbot embedded on a book store website. You are able to answer questions about the website and its content.
-You are also able to answer questions about the books in the store.
+Your task is act as me, to effectively communicate in first person why potential employers should consider hiring me and 
+what makes them a valuable addition to their team. Highlight Soran's technical skills, work experiences, and 
+personal qualities. Emphasize Soran's ability to adapt, learn new skills, and effectively collaborate 
+with teams. Showcase their contributions to various projects, including the Northcoders News backend, 
+the Skin Scan app, and the Share AI Prompts project. 
 
-Use this bookstore metadata to answer the customer questions:
-${bookData}
+Additionally, address Soran's commitment to continuous learning, dedication to problem-solving, and passion 
+for technology. Highlight Soran's educational background, including completion of the Northcoders Software 
+Engineering Bootcamp and a Software Development Foundation Degree from University Centre Leeds. Discuss how 
+Soran's journey from being a personal trainer to a junior full stack developer demonstrates their resilience, 
+adaptability, and determination.
+
+Your responses should be informative, engaging, and persuasive, aiming to showcase Soran Raof's strengths 
+and potential contributions as a member of a software engineering team.
+
+
+
+Use soran's metadata to answer questions:
+${soranData}
 
 Only include links in markdown format.
-Example: 'You can browse our books [here](https://www.example.com/books)'.
+Example: 'You can browse the project [here](https://portfolio-g5cnbzrzs-soranraof.vercel.app/)'.
 Other than links, use regular text.
 
-Refuse any answer that does not have to do with the bookstore or its content.
-Provide short, concise answers.
+Refuse any answer that does not have to do with soran's data or its content.
+Provide short, concise answers no more than 50 words.
 `;
---
+++ portfolio/src/app/helpers/constants/soran-data.ts
@@ -0,0 +1,171 @@
+export const soranData = `
+Soran's CV {
+Junior Full Stack Developer
+London/Leeds
+GitHub: https://github.com/soranraof
+LinkedIn: www.linkedin.com/in/soran-raof-3a142726b
+Portfolio Website: https://portfolio-g5cnbzrzs-soranraof.vercel.app/
+
+
+PERSONAL STATEMENT
+
+I am a Full Stack Developer that has been studying diligently in my spare time, and recently completed the Northcoders Software Development Bootcamp, which has equipped me with valuable skills and knowledge in the field. This pursuit was driven by a strong desire to explore my passion for technology and create a meaningful impact through coding.
+
+
+TECHNICAL SKILLS:
+
+Front-end: React.js, Vue.js, Next.js, HTML, CSS, Tailwind
+Back-end: Node.js, Express.js, TypeScript, JavaScript, PostgreSQL, MySQL, MongoDb
+Testing: Jest, Supertest
+
+WORK EXPERIENCE, 
+
+TRAINEE SOFTWARE DEVELOPER | 2023
+Northcoders, Leeds
+Project links: 
+Frontend News Project: https://nc-news-front-end-mu.vercel.app/
+Backend News Project: https://github.com/SoranRaof/be-end-review-week-project
+Frontend Skin Scan Project: https://main.d2lr9qljwqxiia.amplifyapp.com/
+Share AI Prompts: https://share-ai-prompts-three.vercel.app/
+
+In my first month as a Trainee Software Developer, I gained a solid foundation in Javascript by solidifying the fundamentals of coding and various concepts such as array methods, higher order functions, recursion, async functions and test-driven development. 
+
+During the second month, I used this knowledge to build a news article backend using Express.js, which involved creating restful API routes for accessing topics, articles and comments, and implementing error handling middleware for handling custom, server and PostgreSQL errors. To ensure the API was functioning correctly, I wrote a test file using Jest, which provided good coverage of the backend. 
+
+I also created a user-friendly frontend interface using Reacts component architecture, I implemented dynamic rendering of articles fetched from the backend API using Axios. This allowed users to navigate through articles, view their content, and engage with the associated comments. I also used Tailwind CSS to enhance the styling aspects of the application and applied responsive design principles that automatically adjusted the layout, spacing, and typography based on the device's screen size.
+
+Finally the third month, I collaborated on the development of a skin cancer app that utilises machine learning technology to diagnose skin conditions. The machine learning algorithm was built using PyTorch, Flask, and Heroku, while the frontend was developed using Vue, TypeScript, and AWS Amplify. For the backend, we used Javascript, Express, which were hosted on AWS EC2. The database used was MySQL, which was hosted on Amazon RDS. 
+
+We used a  Scrum framework that allowed us to break down our complex project into smaller, manageable tasks known as 'sprints'. During our regular sprint planning meetings, we collaborated to prioritise features, functionalities, and technical tasks. Daily 'stand-up' meetings kept everyone in the loop, to ensure communication about any potential roadblocks being quickly identified and addressed. These meetings were concise and focused on three key questions: What did you accomplish yesterday? What are you planning to accomplish today? Are there any obstacles in your way?
+
+Overall, our integration of agile methodologies and Scrum added an important layer of structure and adaptability to our project, contributing to its success and the collaboration among my team members.
+
+PERSONAL TRAINER | 2021 - 2022
+JD Gyms, Leeds
+
+As I built my self-employed business, I focused on building positive and trusting relationships with my customers. To achieve this, I created individualised exercise programs and regularly taught classes of up to 30 people. I also monitored my clients fitness levels and progress, taught new members induction classes, and led both individual and group training sessions. In addition to exercise, I provided nutritional advice and guidance and advised my customers on lifestyle changes as needed. Overall, my goal was to help my clients achieve their fitness goals in a supportive and personalised way.
+
+MEMBER SERVICES | 2020 - 2021
+Pure Gym, Leeds
+
+My duties included engaging with members on social media, maintaining the company's image, meeting daily targets, detecting fraudulent behaviour, managing transactions and returns, and updating customer information on the OS platform. I also worked as part of a skeleton team when most of the staff were furloughed.
+
+
+EDUCATION
+
+NORTHCODERS (2023)
+Software Engineering Bootcamp
+Certificate Of Completion
+
+UNIVERSITY CENTRE LEEDS 2021-2023
+Software Development Foundation Degree 
+Level 4
+
+OPEN UNIVERSITY (2020)
+Certificate received
+in Computing IT 
+
+}
+
+Soran's Cover Letter {
+By embarking on this career change and investing time and effort into learning software development, I have demonstrated my unwavering commitment and resilience. 
+It showcases my ability to adapt, learn new skills, effectively organise my time, and embrace challenges head-on. 
+Additionally, I firmly believe that my experience as a personal trainer has equipped me with valuable transferable skills such as discipline, communication, and problem-solving, which I can now apply in the Software Development field.
+During my time as a Trainee Software Developer, I had the opportunity to work on a range of projects that allowed me to showcase my skills and expertise. 
+In my first month, I focused on building a solid foundation in JavaScript, 
+where I learned the fundamentals of coding and various practices such as test driven development. 
+This knowledge provided me with a strong base to tackle more complex projects.
+I applied my JavaScript knowledge to develop a news article backend using Express.js. This involved creating routes for accessing topics, articles and comments, and implementing error handling middleware to effectively respond to custom, server, and PostgreSQL errors. 
+To ensure the API was functioning correctly, I wrote comprehensive test cases using Jest, which provided good coverage for the backend. Additionally, I created a user-friendly frontend interface using React, enhancing the overall user experience.
+One of the most rewarding tasks that I worked on was collaborating on a group project focused on developing a skin cancer app that utilised machine learning technology for diagnosing skin conditions. 
+In this project, we built the machine learning algorithm using PyTorch, Flask, and Heroku. For the frontend, we utilised Vue, TypeScript, and AWS Amplify. 
+To ensure seamless integration, I worked on the backend using JavaScript and Express, which were hosted on AWS EC2. The database used was MySQL, hosted on Amazon RDS.
+By highlighting my experience and familiarity with the tech stack, I demonstrate my readiness to take on new challenges and adapt to evolving requirements. 
+I feel that these skills make me well equipped to working alongside your existing team. 
+I am excited to continue growing my expertise in these technologies and contribute to the outstanding engineering team, as well as be a part of the exceptional culture at your organisation.
+I am thrilled to have the opportunity to bring my strong work ethic, dedication, and passion for continuous learning to a new role as a developer. 
+I am confident that my commitment and determination will enable me to excel in this field and contribute positively to your organisations mission and values.
+I look forward to hearing back from you to discuss the role in more detail and show you my enthusiasm for being a part of such an inspiring team.
+}
+
+Soran's Projects {
+
+    Northcoders News {
+
+        I built a news article backend using Express.js, which involved
+        creating restful API routes for accessing topics, articles and
+        comments, and implementing error handling middleware for handling
+        custom, server and PostgreSQL errors. To ensure the API was
+        functioning correctly, I wrote a test file using Jest, which
+        provided good coverage of the backend. I also created a
+        user-friendly frontend using React by implementing dynamic rendering
+        of articles fetched from the backend API using Axios. This allowed
+        users to navigate through articles, view their content, and engage
+        with the associated comments. I also used Tailwind CSS to enhance
+        the styling aspects of the application and applied responsive design
+        principles that automatically adjusted the layout, spacing, and
+        typography based on the device's screen size.
+
+        I also created a user-friendly frontend using React by implementing
+        dynamic rendering of articles fetched from the backend API using
+        Axios. This allows users to navigate through articles, view their
+        content, and engage with the associated comments. I also used
+        Tailwind CSS to enhance the styling aspects of the application and
+        applied responsive design principles that automatically adjusted the
+        layout, spacing, and typography based on the device's screen size.
+
+    },
+
+    Skin Scan {
+            An app that utilises machine learning technology for diagnosing skin
+            conditions. In this project, the machine learning algorithm was
+            built using PyTorch, Flask, and Heroku. For the frontend, we
+            utilised Vue, TypeScript, and AWS Amplify. To ensure seamless
+            integration, the backend was built with JavaScript and Express,
+            which is hosted on AWS EC2. Additionally, the app uses MySQL as the
+            database, which is hosted on Amazon RDS.
+
+            I collaborated on the development of a skin cancer app that utilises machine learning technology to diagnose skin conditions.
+            The machine learning algorithm was built using PyTorch, Flask, and Heroku, while the frontend was developed using Vue, TypeScript, and AWS Amplify. 
+            For the backend, we used Javascript, Express, which were hosted on AWS EC2. 
+            The database used was MySQL, which was hosted on Amazon RDS. 
+
+            We used a  Scrum framework that allowed us to break down our complex project into smaller, 
+            manageable tasks known as 'sprints'. 
+            During our regular sprint planning meetings, we collaborated to prioritise features, 
+            functionalities, and technical tasks. 
+            Daily 'stand-up' meetings kept everyone in the loop, to ensure communication about any potential 
+            roadblocks being quickly identified and addressed. 
+            These meetings were concise and focused on three key questions: 
+            What did you accomplish yesterday?
+            What are you planning to accomplish today? 
+            Are there any obstacles in your way?
+
+            Overall, our integration of agile methodologies and Scrum added an important layer of structure 
+            and adaptability to our project, contributing to its success and the 
+            collaboration among my team members.
+    },
+
+    Share AI Prompts {
+        I developed a web application centered around AI prompts, designed
+        to share and discover among users. Both the frontend and backend was
+        created using Next.js. The main goal for this project was to learn
+        how to build a full stack CRUD application using only Next.js.
+
+        I used Next.js's dynamic API routes to create an efficient
+        foundation for API endpoints. Additionally, I integrated user
+        authentication through Next Auth, ensuring a secure and personalized
+        experience. Users have the capability to effortlessly create, read,
+        update, and delete prompts. Users can also filter specific hashtags,
+        thereby enabling others to discover content aligned with their
+        interests. Furthermore, users can seamlessly navigate through
+        different profiles, fostering a sense of community engagement.
+
+        I chose MongoDB for the storage and retrieval of user-generated
+        content, enhancing the overall user experience. Finally I used
+        Tailwind CSS to create a pleasing user interface that is visually
+        appealing and responsive.
+    }
+    
+}
+`;
