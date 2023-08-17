5c1b2ff3c149f1751e4d2cd8dbe50482c7824c21
Soran Raof <soranraof@hotmail.com>
2023-08-17 14:11:43 +0100

    chatbot name

--- portfolio/src/app/components/chatbot/ChatHeader.tsx
+++ portfolio/src/app/components/chatbot/ChatHeader.tsx
@@ -9,7 +9,7 @@ const ChatHeader: FC<ChatHeaderProps> = ({}) => {
         <p className="text-xs">Ask me some questions</p>
         <div className="flex gap-1.5 items-center">
           <p className="w-2 h-2 rounded-full bg-green-500"></p>
-          <p className="font-medium">Soran GPT</p>
+          <p className="font-medium">Soran</p>
         </div>
       </div>
     </div>
