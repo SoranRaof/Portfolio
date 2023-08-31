febdf0d5adfb349d8fb1a45ea52dfd6b99b80d03
Soran Raof <110354376+SoranRaof@users.noreply.github.com>
2023-08-31 12:40:19 +0100

    rename buttons

--- portfolio/src/app/ncnewsfrontend/page.jsx
+++ portfolio/src/app/ncnewsfrontend/page.jsx
@@ -59,7 +59,7 @@ export default function NcNewsFrontEnd() {
             rel="noreferrer"
           >
             <button className="px-8 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300">
-              Code
+              Frontend
             </button>
           </a>
           <a
